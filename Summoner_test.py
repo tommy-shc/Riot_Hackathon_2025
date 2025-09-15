36b27f0cb2e737506bb2ee9525767a59a180bb25
Jason Wu <wujason19@gmail.com>
2025-09-15 19:00:15 -0400

    testing tommy's account lol

---
+++ Summoner_test.py
@@ -0,0 +1,29 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+APIKEY  = os.getenv("APIKEY")
+
+class Summoner:
+    def __init__(self, gameName, tagLine="NA1"):
+        self.gameName = gameName
+        self.tagLine = tagLine
+        self.account = self.get_summoner_by_name()
+        self.puuid = self.account['puuid']
+
+    def get_summoner_by_name(self):
+        url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{self.gameName}/{self.tagLine}?api_key=" + APIKEY
+
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error: {response.status_code}")
+            return None
+
+summoner = Summoner("dump1ng0d", "lol")
+
+
+print(summoner.puuid)
